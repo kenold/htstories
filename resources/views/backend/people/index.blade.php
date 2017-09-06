7a3ab35cfa8b98485c11b64a9e789794159bbbc7
Kenold Beauplan <bkenold@gmail.com>
2017-09-06 15:33:30 -0400

    display pagination

--- resources/views/backend/people/index.blade.php
+++ resources/views/backend/people/index.blade.php
@@ -1,5 +1,13 @@
-<ul>
-    @foreach($people as $person)
-        <li>{{ $person->name }}</li>
-    @endforeach
-</ul>
+@extends('backend.layouts.master')
+
+@section('content')
+    <ul class="list-unstyled">
+        @foreach($people as $person)
+            <li>{{ $person->name }}</li>
+        @endforeach
+    </ul>
+
+    <nav aria-label="Page navigation">
+        {{ $people->links() }}
+    </nav>
+@endsection    
